a800ccc17c34db06ba29059ee333d15fbb6eb12b
Holly Leary <schleary@gmail.com>
2014-11-10 08:53:35 -0800

    some tests

--- holly/spec/controllers/measurement_spec.rb
+++ holly/spec/controllers/measurement_spec.rb
@@ -12,11 +12,4 @@ describe "Measurement" do
     expect(join.quantity).to eq "7"
   end
 
-  # it "can create a recipe_form with a recipe, ingredients, and quantity measurements" do
-  #   # @recipe_form = RecipeForm.new("recipe"=>{"title"=>"Turtle", "ingredients"=>["1"], "measurements"=>["", "", "", "", "", ""], "ingredients_index"=>["1", "2", "3", "4", "5", "6"], "directions"=>"Stir Stuff", "image"=>"Leak"}, "commit"=>"Save", "controller"=>"recipes", "action"=>"create")
-  #   # @recipe = Recipe.create(title: @attributes[:recipe][:title], directions: @attributes[:recipe][:directions], image: @attributes[:recipe][:image])
-  #   #
-  #   # expect (@recipe.directions).to eq "Stir Stuff"
-  # end
-
 end
